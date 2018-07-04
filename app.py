f02247c179cc3c4ac6b33484e4108f10266a6bd6
Seth Drew <seth@sethdrew.com>
2018-07-04 16:20:35 -0400

    Base flask for simple d3

---
+++ app.py
@@ -0,0 +1,18 @@
+
+from flask import Flask
+from flask import render_template, request, jsonify
+
+app = Flask(__name__)
+
+
+
+
+@app.route('/')
+def index():
+
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8000)
+    
